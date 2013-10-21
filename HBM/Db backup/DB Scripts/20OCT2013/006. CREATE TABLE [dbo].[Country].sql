69a7d392768ac938d271a6a1461ec8f00537c9b2
Nirshan Dileep <cooldilcool@gmail.com>
2013-10-20 19:40:35 -0700

    DB Changes related to country

---
+++ HBM/Db backup/DB Scripts/20OCT2013/006. CREATE TABLE [dbo].[Country].sql
@@ -0,0 +1,20 @@
+/****** Object:  Table [dbo].[Country]    Script Date: 10/21/2013 08:06:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Country](
+	[CountryId] [int] IDENTITY(1,1) NOT NULL,
+	[CountryCode] [nchar](2) NOT NULL,
+	[Country] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED 
+(
+	[CountryId] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
