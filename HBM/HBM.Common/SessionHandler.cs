b3a9b2a80b5f6294b6d813e80921dcfac31def57
Nirshan Dileep <cooldilcool@gmail.com>
2013-10-30 19:57:03 -0700

    Session Handler Added

---
+++ HBM/HBM.Common/SessionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.SessionState;
+using System.Web;
+
+namespace HBM.Common
+{
+    public class SessionHandler : IRequiresSessionState
+    {
+
+
+        public static T GetSession<T>(string sessionName) where T : new()
+        {
+            T entity = default(T);
+
+            string TypeName = typeof(T).Name;
+
+            if (HttpContext.Current.Session[sessionName] != null)
+            {
+                entity = (T)(HttpContext.Current.Session[sessionName]);
+            }
+
+            return entity;
+        }
+
+        public void getSession(string key)
+        {
+            if (HttpContext.Current.Session[key] != null)
+            {
+                string sessionID = HttpContext.Current.Session[key].ToString();
+            }
+        }
+    }
+}
